09f6b61755ec0928139d7e48b61ba324d9edd34d
godhatesaustralia <asybad69@gmail.com>
2024-05-26 13:14:03 -0400

    works

--- Display/Display.cs
+++ Display/Display.cs
@@ -317,6 +317,9 @@ namespace IngameScript
                     Outputs.Add(sc.Name, sc);
                     if (i == 0)
                         ActiveScreens.Add(index, sc.Name);
+                    if (ScreenNames.ContainsKey(index))
+                        ScreenNames[index].Add(sc.Name);
+                    else ScreenNames[index] = new HashSet<string> { sc.Name };
                 }
                 return good;
             }
@@ -397,20 +400,13 @@ namespace IngameScript
 
         public void MFDSwitch(int i, string n)
         {
-            string test = "";
-            foreach (var key in ScreenNames.Keys)
-            {
-                test += $"\n\n{key}";
-                if (ScreenNames[key].Count != 0)
-                    foreach (var n2 in ScreenNames[key])
-                        test += $"\n{n2}";
-            }
-            throw new Exception($"\nswitch executed {Name} {i}-{n} populated {ScreenNames.Count != 0} test {test}");
             if (!(ScreenNames.ContainsKey(i) && ScreenNames[i].Contains(n)))
                 return;
-            
             ActiveScreens[i] = n;
-
+            var f = Outputs[n].dsp.DrawFrame();
+            foreach (var s in Outputs[n].Sprites.Values)
+                f.Add(s.Sprite);
+            f.Dispose();
         }
 
 
